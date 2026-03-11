2b6f90a4b367d29689152e935ea3d43e0542b2d5
Idan Shamam <idan@starkware.co>
2026-03-11 20:55:40 +0200

    Fix gatekeeper 3

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -2,7 +2,9 @@ package cli
 
 import (
 	"context"
+	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/spf13/cobra"
@@ -10,8 +12,18 @@ import (
 
 // These variables will be set by command line flags.
 var (
-	ghToken string
+	ghToken    string
+	flagDebug  bool
 )
+
+// isDebugEnabled returns true if debug logging should be enabled (--debug or GitHub Actions debug env).
+func isDebugEnabled() bool {
+	if flagDebug {
+		return true
+	}
+	return strings.EqualFold(os.Getenv("ACTIONS_STEP_DEBUG"), "true") ||
+		strings.EqualFold(os.Getenv("ACTIONS_RUNNER_DEBUG"), "true")
+}
 
 func Run(version string, args ...string) error {
 	cmd := &cobra.Command{
@@ -20,6 +32,7 @@ func Run(version string, args ...string) error {
 		Version: version,
 	}
 	cmd.PersistentFlags().StringVarP(&ghToken, "token", "t", "", "GitHub token (or set GITHUB_TOKEN env)")
+	cmd.PersistentFlags().BoolVar(&flagDebug, "debug", false, "enable debug logging (also enabled when ACTIONS_STEP_DEBUG or ACTIONS_RUNNER_DEBUG is true)")
 
 	cmd.AddCommand(validateCmd())
 
